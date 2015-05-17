57c494b27ec3091dd4f6346fe58183836b35b390
River Marmorstein <river@rmarmorstein.com>
2015-05-16 23:43:47 -0400

    merge conflicts?

--- src/main/java/org/vizun/Vizun.java
+++ src/main/java/org/vizun/Vizun.java
@@ -1,13 +1,10 @@
 package org.vizun;
 
-<<<<<<< HEAD
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import  org.vizun.engine.display.Displaymanager;
-=======
 import org.vizun.engine.config.Configuration;
 import org.vizun.engine.display.Displaymanager;
->>>>>>> 3a0773e4ca524ceec81f69f2e394565728896393
 
 import org.lwjgl.opengl.Display;
 import org.vizun.engine.loader.Loader;
@@ -20,17 +17,14 @@ import org.vizun.engine.util.language;
 import org.vizun.engine.model.RawModel;
 
 public class Vizun {
-    
-<<<<<<< HEAD
-    public static Displaymanager displayManager;
-    
+
     private static Logger logger;
-=======
+
     private static Displaymanager displayManager;
+
     private static final org.vizun.engine.render.masterRenderer masterRenderer = new masterRenderer();
     private static final language lang = new language(new Configuration("src/main/resources/configurations/english.json"));
     private static final Loader loader = new Loader();
->>>>>>> 3a0773e4ca524ceec81f69f2e394565728896393
 
     public static void main(String[] args){
         displayManager = new Displaymanager(800, 600, 120, "Vizun");
