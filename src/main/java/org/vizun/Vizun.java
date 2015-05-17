c2b6f55b3547c49a3b09e649e7f226b15e9a1523
River Marmorstein <river@rmarmorstein.com>
2015-05-17 02:23:07 -0400

    fix references, json update

--- src/main/java/org/vizun/Vizun.java
+++ src/main/java/org/vizun/Vizun.java
@@ -2,18 +2,21 @@ package org.vizun;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.vizun.engine.config.Configuration;
+import org.vizun.engine.config.Config;
+import org.vizun.engine.config.JSONConfiguration;
 import  org.vizun.engine.display.Displaymanager;
 
 import org.lwjgl.opengl.Display;
 import org.vizun.engine.util.language;
+
+import java.io.File;
 
 public class Vizun {
     
     public static Displaymanager displayManager;
     
     private static Logger logger;
-    private static final language lang = new language(new Configuration("src/main/resources/configurations/spanish.json"));
+    private static final language lang = new language(new JSONConfiguration(new File("src/main/resources/configurations/spanish.json")));
 
     public static void main(String[] args){
         displayManager = new Displaymanager(800, 600, 120, "Vizun");
